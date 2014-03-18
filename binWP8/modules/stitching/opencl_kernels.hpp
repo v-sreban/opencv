faf66cc14d65d23b776c4dd2909647677f20bb66
Dale Stammen <dalestam@microsoft.com>
2014-03-18 14:52:05 -0700

    added module projects for wp8

---
+++ binWP8/modules/stitching/opencl_kernels.hpp
@@ -0,0 +1,16 @@
+// This file is auto-generated. Do not edit!
+
+#include "opencv2/core/ocl_genbase.hpp"
+#include "opencv2/core/opencl/ocl_defs.hpp"
+
+namespace cv
+{
+namespace ocl
+{
+namespace stitching
+{
+
+extern const struct ProgramEntry warpers;
+extern ProgramSource warpers_oclsrc;
+}
+}}
