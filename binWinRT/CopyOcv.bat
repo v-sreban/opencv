791a0ee02d26417506f37c1193055ce3823b1371
Neil McLaughlin <v-nemcla@microsoft.com>
2014-04-10 10:16:00 -0500

    Updated path from opencv-pr to (pointer to) opencv

--- binWinRT/CopyOcv.bat
+++ binWinRT/CopyOcv.bat
@@ -1,5 +1,5 @@
 REM usage: run from github directory 
 
-call CopyOcvIncludes ..\..\openframeworks\addons\ofxOpenCv\libs\opencv\include ..\..
-call CopyOcvLibs ..\.. openframeworks
+call CopyOcvIncludes ..\..\openframeworks\addons\ofxOpenCv\libs\opencv\include ..\.. opencv
+call CopyOcvLibs ..\.. openframeworks opencv
 
--- binWinRT/CopyOcvLibs.bat
+++ binWinRT/CopyOcvLibs.bat
@@ -1,33 +1,33 @@
-md %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt
-md %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug
-md %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release
+md %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt
+md %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug
+md %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release
 
-copy %1\opencv-pr\binWinRT\ARM\Debug\zlib\zlib.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_calib3d\opencv_calib3d.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_contrib\opencv_contrib.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_core\opencv_core.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_features2d\opencv_features2d.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_flann\opencv_flann.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_imgproc\opencv_imgproc.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_legacy\opencv_legacy.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_ml\opencv_ml.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_objdetect\opencv_objdetect.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_photo\opencv_photo.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_stitching\opencv_stitching.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_video\opencv_video.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
-copy %1\opencv-pr\binWinRT\ARM\Debug\opencv_videostab\opencv_videostab.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\zlib\zlib.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_calib3d\opencv_calib3d.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_contrib\opencv_contrib.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_core\opencv_core.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_features2d\opencv_features2d.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_flann\opencv_flann.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_imgproc\opencv_imgproc.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_legacy\opencv_legacy.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_ml\opencv_ml.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_objdetect\opencv_objdetect.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_photo\opencv_photo.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_stitching\opencv_stitching.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_video\opencv_video.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
+copy %1\%3\binWinRT\ARM\Debug\opencv_videostab\opencv_videostab.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Debug /y
 
-copy %1\opencv-pr\binWinRT\ARM\Release\zlib\zlib.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_calib3d\opencv_calib3d.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_contrib\opencv_contrib.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_core\opencv_core.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_features2d\opencv_features2d.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_flann\opencv_flann.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_imgproc\opencv_imgproc.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_legacy\opencv_legacy.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_ml\opencv_ml.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_objdetect\opencv_objdetect.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_photo\opencv_photo.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_stitching\opencv_stitching.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_video\opencv_video.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
-copy %1\opencv-pr\binWinRT\ARM\Release\opencv_videostab\opencv_videostab.lib %1\openframeworks\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\zlib\zlib.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_calib3d\opencv_calib3d.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_contrib\opencv_contrib.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_core\opencv_core.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_features2d\opencv_features2d.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_flann\opencv_flann.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_imgproc\opencv_imgproc.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_legacy\opencv_legacy.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_ml\opencv_ml.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_objdetect\opencv_objdetect.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_photo\opencv_photo.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_stitching\opencv_stitching.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_video\opencv_video.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
+copy %1\%3\binWinRT\ARM\Release\opencv_videostab\opencv_videostab.lib %1\%2\addons\ofxOpenCV\libs\opencv\lib\winrt\Release /y
