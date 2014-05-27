fb9854af70d9dfb204e973b06f71c6bb60a7ae7f
Dave Remba <daveremba@yahoo.com>
2014-05-26 19:35:13 -0700

    added copyright noteice and m_capture

--- modules/highgui/src/cap_winrt_highgui.hpp
+++ modules/highgui/src/cap_winrt_highgui.hpp
@@ -1,3 +1,32 @@
+// Capture support for WinRT - bridge from OpenCV to XAML
+// Microsoft Surface and Phone using Media Foundation
+
+// Copyright (c) 2013, Microsoft Open Technologies, Inc. 
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, 
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+// - Neither the name of Microsoft Open Technologies, Inc. nor the names 
+//   of its contributors may be used to endorse or promote products derived 
+//   from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <ppl.h>
 #include <concrt.h>
@@ -7,6 +36,7 @@
 //using namespace ::concurrency;
 //using namespace ::Windows::Foundation;
 
+#if 0
 // this is an OpenCV class
 // stub
 namespace cv
@@ -15,7 +45,7 @@ namespace cv
     {
     };
 }
-
+#endif
 
 enum {
     HighguiBridge_OPEN_CAMERA = 300,
@@ -34,24 +64,17 @@ public:
         static HighguiBridge instance;
         return instance;
     }
-    void perform(int action) { reporter.report(action); }
-    void setReporter(Concurrency::progress_reporter<int> pr) { reporter = pr; }
+    // void perform(int action) { reporter.report(action); }
+    // void setReporter(Concurrency::progress_reporter<int> pr) { reporter = pr; }
 
     // highgui video interface
-    bool initializeDevice() {
-        // store capture = ref new MediaCapture...
-        // call init capture device async using deviceIndex
-        // .then( [] { ...
-        deviceReady = true;
-        return true;
-        // });
-    }
+    bool initializeDevice();
 
     // highgui UI interface
     void createTrackbar( /* callbackOnChange */) {} // unhides trackbar and registers OpenCV callback
     void setTrackbarPos(int pos) {}                 // unhides trackbar and sets its position
 
-    void imshow(cv::Mat matToShow) {}                   // shows Mat in the cvImage element
+    void imshow(/*cv::Mat matToShow*/) {}                   // shows Mat in the cvImage element
     // namedWindow                                  // no op: only one XAML window used
     // destroyWindow                                // no op
     // destroyAllWindows                            // no op
@@ -76,9 +99,14 @@ private:
     HighguiBridge() {
         deviceReady = false;
     };
+
     HighguiBridge(HighguiBridge const &);
     void operator=(const HighguiBridge &);
 
-    Concurrency::progress_reporter<int> reporter;
+    // Concurrency::progress_reporter<int> reporter;
     std::atomic<bool> deviceReady;
+
+//    Platform::Agile<::Windows::Media::Capture::MediaCapture> m_capture;
+//    Platform::Agile<Windows::Devices::Enumeration::DeviceInformationCollection> m_devices;
+
 };
